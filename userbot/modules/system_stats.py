8f046c973087eed484a6a7041b2e85f02b01bb1a
BryanAsf <80415153+bryanasfuk@users.noreply.github.com>
2021-03-15 01:20:20 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -233,10 +233,10 @@ async def amireallyalive(alive):
     output = (
         f" ✟ ᴍᴀʏ ꜱᴀᴛᴀɴ ʙʟᴇꜱꜱ ʏᴏᴜ ✟\n\n"
         f" `☨ ꜱɪɴɴᴇʀ     ⇎ `{DEFAULTUSER}\n"
-        f" `☨ ᴛᴇʟᴇᴛʜᴏɴ   ⇎ `ᴠ {version.__version__}\n"
-        f" `☨ ᴘʏᴛʜᴏɴ     ⇎ `ᴠ {python_version()}\n"
-        f" `☨ ᴠᴇʀꜱɪᴏɴ    ⇎ `{BOT_VER}\n"
-        f" `☨ ᴜᴘᴛɪᴍᴇ     ⇎ `{uptime}\n\n"
+        f" `☨ ᴛᴇʟᴇᴛʜᴏɴ    ⇎ `ᴠ {version.__version__}\n"
+        f" `☨ ᴘʏᴛʜᴏɴ    ⇎ `ᴠ {python_version()}\n"
+        f" `☨ ᴠᴇʀꜱɪᴏɴ     ⇎ `{BOT_VER}\n"
+        f" `☨ ᴜᴘᴛɪᴍᴇ    ⇎ `{uptime}\n\n"
         f" ✟ ᴍᴀʏ ꜱᴀᴛᴀɴ ʙʟᴇꜱꜱ ʏᴏᴜ ✟\n"
     )
     if ALIVE_LOGO:
