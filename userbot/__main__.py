9edd4e5e0697a91f9c91c104615ae1bda0c1a4a1
BryanAsf <80415153+bryanasfuk@users.noreply.github.com>
2021-03-15 01:38:51 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"Yama V{BOT_VER} [Baphomet Bot Sudah Bangkit]")
+    f"Baphomet V{BOT_VER} [Baphomet Bot Sudah Bangkit]")
 
 
 if len(argv) not in (1, 3, 4):
