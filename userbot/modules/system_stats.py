301849b586b13f1dcf655db9f170562a77bbdb3c
BryanAsf <80415153+bryanasfuk@users.noreply.github.com>
2021-03-15 01:25:51 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -232,10 +232,10 @@ async def amireallyalive(alive):
     uptime = await get_readable_time((time.time() - StartTime))
     output = (
         f" ✟ ᴍᴀʏ ꜱᴀᴛᴀɴ ʙʟᴇꜱꜱ ʏᴏᴜ ✟\n\n"
-        f" `☨ ꜱɪɴɴᴇʀ     ⇎ `{DEFAULTUSER}\n"
-        f" `☨ ᴛᴇʟᴇᴛʜᴏɴ    ⇎ `ᴠ {version.__version__}\n"
+        f" `☨ ꜱɪɴɴᴇʀ   ⇎ `{DEFAULTUSER}\n"
+        f" `☨ ᴛᴇʟᴇᴛʜᴏɴ  ⇎ `ᴠ {version.__version__}\n"
         f" `☨ ᴘʏᴛʜᴏɴ    ⇎ `ᴠ {python_version()}\n"
-        f" `☨ ᴠᴇʀꜱɪᴏɴ     ⇎ `{BOT_VER}\n"
+        f" `☨ ᴠᴇʀꜱɪᴏɴ   ⇎ `{BOT_VER}\n"
         f" `☨ ᴜᴘᴛɪᴍᴇ    ⇎ `{uptime}\n\n"
         f" ✟ ᴍᴀʏ ꜱᴀᴛᴀɴ ʙʟᴇꜱꜱ ʏᴏᴜ ✟\n"
     )
