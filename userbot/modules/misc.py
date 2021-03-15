808fbb6da5ac03dd0142b081879fb0920973e859
BryanAsf <80415153+bryanasfuk@users.noreply.github.com>
2021-03-15 10:31:32 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -120,7 +120,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "[Baphomet](https://github.com/bryanasfuk/Baphomet)\n"
+        "[[Baphomet]](https://github.com/bryanasfuk/Baphomet)\n"
     )
 
 
